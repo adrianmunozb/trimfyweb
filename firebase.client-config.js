2dddfae2021bb06be3a1e0e0b06add9d8f7e66d3
Adrián Muñoz <adrianmunozborbon@gmail.com>
2026-03-04 19:35:29 +0100

    link good

---
+++ firebase.client-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA25J2xkEXeI4AwuB9akrYJtaZoTW4Z1z8",
+  authDomain: "trimfyweb.firebaseapp.com",
+  projectId: "trimfyweb",
+  storageBucket: "trimfyweb.firebasestorage.app",
+  messagingSenderId: "1038061415008",
+  appId: "1:1038061415008:web:38a3d6ba6983d0ee647105"
+};
